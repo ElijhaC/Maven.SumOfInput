6cac645fc3a4df458b93aa021f7fa830ed9cac1b
elijhac <ej9314@hotmail.com>
2022-10-30 23:02:15 -0400

    Finished with no extra credit

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,16 @@
+import java.util.Scanner;
+
 /**
  * Created by iyasuwatts on 10/17/17.
  */
 public class Main {
 
     public static void main(String[] args){
+        Scanner in = new Scanner(System.in);
+        System.out.println("Please pick a number");
+        int userNum = in.nextInt();
+        int sum = userNum + 1;
+        System.out.println(sum);
 
     }
     
